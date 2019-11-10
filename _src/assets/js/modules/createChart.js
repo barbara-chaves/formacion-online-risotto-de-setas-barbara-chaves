fd1f35f9dc72dd11d204f10f189084ea69ed8671
barbara-chaves <barbarabarcellos90@gmail.com>
2019-11-10 22:23:24 +0100

    add styles

--- _src/assets/js/modules/createChart.js
+++ _src/assets/js/modules/createChart.js
@@ -2,25 +2,29 @@ const buy = document.querySelector(".buy-section");
 
 export default function createChart({currency, items, subtotal, shippingCost, total}) {
   const chart = document.createElement("div");
-  chart.classList.add("chart");
-  chart.innerHTML = `<div class="container total">
-      <small id="items">
-        <span >Items: </span>
-        ${items}
-      </small>
+  chart.classList.add("chart", "card-body", "pr-5", "pl-5");
+  chart.innerHTML = `<div class="total">
+      <div class='pb-4'>
+        <small id="items">
+          <span >Items: </span>
+          ${items}
+        </small>
+      </div>
       <p class='d-flex justify-content-between' id="subtotal">
         <span>Subtotal </span>
-        ${subtotal}${currency}
+        ${subtotal} ${currency}
       </p>
       <p class='d-flex justify-content-between' id="send-charges">
-        <span>Gastos de envío: </span> 
-        ${shippingCost}${currency}
+        <span>Gastos de envío </span> 
+        ${shippingCost} ${currency}
       </p>
       <p class='d-flex justify-content-between' id="total">
-        <span>Total: </span>
-        ${total}${currency}
+        Total 
+        <span class='h5 text-success'>${total} ${currency}</span>
       </p>
-      <button class="btn btn-success" id="buy">Comprar ingredienes ${total}${currency}</button>
+      <div d-flex align-items-stretch>
+        <button class="btn btn-success w-100" id="buy">Comprar ingredienes: ${total} ${currency}</button>
+      </div>
     </div>`;
   buy.appendChild(chart);
 }
--- _src/assets/js/modules/createIngredient.js
+++ _src/assets/js/modules/createIngredient.js
@@ -11,21 +11,21 @@ export default function createIngredient({
 }, currency) {
   const isChecked = () => checked ? "checked" : "";
   const ingredient = document.createElement("li");
-  ingredient.classList.add("list-group");
+  ingredient.classList.add("list-group-item", "p-2");
 
   ingredient.innerHTML = (
     `<div id=${id} class="row item">
-      <div class="col-1">
+      <div class="col-1 ml-2 p-0 d-flex justify-content-center align-items-center">
         <input class='item__checkbox' type="checkbox" ${isChecked()}  />
       </div>
-      <div class="col-2">
-        <input class='item__items' type="number" min='0' value=${parseInt(
+      <div class="col-1 p-0 d-flex align-items-center">
+        <input class='item__items text-primary text-center' type="number" min='0' value=${parseInt(
           items
-        )} style="width: 40px"/>
+        )} style="width: 30px; heigth: 30px" />
       </div>
-      <div class="col-5">
+      <div class="col-4">
         <div class='row'>
-          <p class='item__name'>${product}</p>
+          <p class='item__name m-0'>${product}</p>
         </div>
         <div class='row'>
           ${brand ? `<small class='item__brand'>${brand}</small>` : ""}
@@ -34,8 +34,8 @@ export default function createIngredient({
           <small class='item__quantity'>${quantity}</small>
         </div>
       </div>
-      <div class='col-4' >
-        <p class='text-right item__price'>${cost}${currency}</p>
+      <div class='col-5 pr-5  d-flex justify-content-end align-items-center' >
+        <p class='text-right item__price m-0 text-success'>${cost} ${currency}</p>
       </div>
     </div>
    `);
