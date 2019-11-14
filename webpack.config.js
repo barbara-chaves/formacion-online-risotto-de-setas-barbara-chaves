b9e3fa38cf4be683d98acec955dd41fcff42bef3
barbara-chaves <barbarabarcellos90@gmail.com>
2019-11-14 12:20:45 +0100

    add homepage

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: ['@babel/polyfill', './src/assets/js/main.js'],
   output: {
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "dist"),
     filename: "js/main.js"
   },
   plugins: [
